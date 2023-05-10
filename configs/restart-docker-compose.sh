8c7d399fa519745f5a7f4e8ff136db3144b8fd6a
dakta <763058991@qq.com>
2023-05-10 13:16:48 +0800

    upd conf

--- configs/restart-docker-compose.sh
+++ configs/restart-docker-compose.sh
@@ -1,5 +1,6 @@
 for i in {0..5} ; do
-  rm -rf ./800$i/data;
+  rm -rf ./800"$i"/data;
 done
+# shellcheck disable=SC2046
 docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q)
 docker-compose up -d
